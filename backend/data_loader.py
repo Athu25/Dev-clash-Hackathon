503720d9e1855f8c2be8e78ceff4a6613ae0a6fd
2411Aditya <adityakulkarni2411@gmail.com>
2025-04-05 10:31:54 -0700

    Dataset extended
    Volume of the dataset increased to get more real time accurate predictions

--- backend/data_loader.py
+++ backend/data_loader.py
@@ -26,12 +26,12 @@ from data_loader import fetch_data
 
 # Define parameters
 ticker = 'AAPL'
-start = '2025-01-01'
+start = '2024-01-01'
 end = '2025-03-31'
 
 # Fetch data and save to CSV
 data_frame = fetch_data(ticker, start, end, save_to_csv=True)
 
 # The data is now saved in the 'csv' folder and also available as a DataFrame
-print(data_frame.head(50))
+print(data_frame.head(5))
 
