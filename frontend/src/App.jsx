9858255201bff2b1c19a2240e2cfb91c26663f08
Atharva Kshirsagar <kshirsagaratharva250@gmail.com>
2025-04-06 07:24:03 +0530

    App.jsx update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import CandlestickChart from "./components/CandlestickChart";
+import CandlestickChart from "./components/CandleStickChart";
 import "./index.css";
 
 const App = () => {
@@ -7,10 +7,8 @@ const App = () => {
     <div className="min-h-screen bg-gray-900 text-white p-6">
       <h1 className="text-4xl font-bold mb-6 text-center">Trading Dashboard</h1>
 
-      <div className="max-w-6xl mx-auto bg-gray-800 p-6 rounded-2xl shadow-lg">
-        <div className="bg-blue-500 text-white p-4 text-center">Chart Should Render Below 👇</div>
+      
         <CandlestickChart />
-      </div>
     </div>
   );
 };
